e6439c42fd8aa8b1a19233f1ad1b74153d56f435
ofersomberg-dev <ofersomberg@gmail.com>
2025-11-12 07:24:00 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,6 +1,6 @@
 def approximate_pi(n_terms):
     lebianese = []
     for i in range(n_terms):
-        term = ((-1) ** i) / (2 * i + 1)
+        thing = ((-1) ** i) / (2 * i + 1)
         lebianese.append(thing)
     return (sum(lebianese)*4)
